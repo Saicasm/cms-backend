11d9e636ab6979f9ef1c5ebfc92e1f397bf3a0dd
Sai <sai98nsk@gmail.com>
2020-10-25 17:18:32 +0530

    added comfig  for pre commit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const UserController = require("../controllers/userController");
+const express = require('express');
+const UserController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get("/", UserController.getAllUsers);
-router.post("/", UserController.addUser);
-router.get("/:id", UserController.getUser);
-router.put("/:id", UserController.updateUser);
-router.delete("/:id", UserController.deleteUser);
+router.get('/', UserController.getAllUsers);
+router.post('/', UserController.addUser);
+router.get('/:id', UserController.getUser);
+router.put('/:id', UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
